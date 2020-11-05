445bb4e4b108caf79d671485faeaacbefb7f387d
Abhishek K T <39241612+Abhishekkt15@users.noreply.github.com>
2020-11-05 13:25:59 +0530

    Update slist.c

--- Single_Linked_List/slist.c
+++ Single_Linked_List/slist.c
@@ -1,1 +1,36 @@
+#include<stdlib.h>
+#include<stddef.h>
+#include<assert.h>
+#include "slist.h"
+#include<stdio.h>
+Slist slist_new()
+{
+    Slist s={NULL,NULL,0};
+    return s;
+}
 
+
+
+static Node* slist_new_node(int32_t element)
+{
+    Node *new_node=(Node *)malloc(sizeof(Node));
+    new_node -> data=element;
+    new_node->next=NULL;
+    return new_node;
+}
+
+
+Slist* slist_add_head(Slist *list,int32_t element)
+{
+assert(list!=NULL);
+Node *new_node=slist_new_node(element);
+new_node->next=list->head;
+list->head=new_node;
+if(list->tail ==NULL)
+    {
+        list->tail=new_node;
+    }
+++list->length;
+assert((list->length==1 && list->head==list->tail) || (list->length>0 && list->head!=list->tail));
+return list;
+}
