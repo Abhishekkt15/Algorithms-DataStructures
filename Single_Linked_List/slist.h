ebf1a42003fd333395cf06442991dd12d02352bb
Abhishek K T <39241612+Abhishekkt15@users.noreply.github.com>
2020-11-05 13:22:35 +0530

    Update slist.h

--- Single_Linked_List/slist.h
+++ Single_Linked_List/slist.h
@@ -1,1 +1,20 @@
+#ifndef _INCLUDED_SLIST
+#define _INCLUDED_SLIST
+#include<stdint.h>
 
+
+typedef struct _node_ Node;
+typedef struct _slist_ Slist;
+
+
+struct _node_{
+int32_t data;
+Node *next;
+};
+
+
+struct _slist_{
+Node *head;
+Node *tail;
+uint32_t length;
+};
